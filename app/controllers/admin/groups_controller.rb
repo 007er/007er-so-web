719af3ee5cf8aae14bf6093df944f6f9d6041d2d
sida <siningdream@gmail.com>
2017-08-13 22:10:06 +0800

    Add admin/groups#create. Enable admin to create a group

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -7,4 +7,19 @@ class Admin::GroupsController < Admin::AdminsController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to admin_groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
