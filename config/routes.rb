df97ba2de4673ccdf83e1bf4dd9084625a617604
sida <siningdream@gmail.com>
2017-08-13 22:10:53 +0800

    Add groups#index to enable view all groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root "groups#index"
   devise_for :admins
+
   get 'admin', to: 'admin/admins#index'
   namespace :admin do
     resources :groups
   end
+
+  resources :groups, only: [:index]
 end
