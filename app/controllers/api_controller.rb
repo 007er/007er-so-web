b57a54790f49ed53e564d8da99e71cd38d914c79
liubingjie <breeder1121@hotmail.com>
2017-09-30 21:50:19 +0800

    create post api v1

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,7 @@ class ApiController < ActionController::Base
 	def authenticate_user_from_token!
 
 		if params[:auth_token].present?
-			user = User.find_by_authentication_token( params[:quth_token] )
+			user = User.find_by_authentication_token( params[:auth_token] )
 			
 			#sign_in 是 Devise 的方法， 会设定好 current_user
 			sign_in(user, store: false) if user
